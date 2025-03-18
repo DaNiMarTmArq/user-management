fb571db3274e43663be769dba2d395554d3df570
Daniel Martinez <danimartinezmarquez@gmail.com>
2025-03-18 19:35:01 +0100

    Basic routes setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: DashboardComponent },
+  { path: 'user/:id', component: DetailComponent },
+  { path: '**', component: NotfoundComponent },
+];
