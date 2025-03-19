bdbb1c65184febdc58589bd2317ceab7949b2d3d
Daniel Martinez <danimartinezmarquez@gmail.com>
2025-03-19 20:07:52 +0100

    First version of the dashboard without pagination

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { UserComponent } from './user/user/user.component';
+import { User } from '../../Interfaces/User';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +9,21 @@ import { UserComponent } from './user/user/user.component';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  userList: User[] = [];
+  currentPage = 0;
+  userService = inject(UserService);
+
+  ngOnInit() {
+    this.userService.getAll().subscribe({
+      next: (list) => this.handleUserResponse(list),
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  private handleUserResponse(list: User[]) {
+    this.userList = list;
+  }
+}
