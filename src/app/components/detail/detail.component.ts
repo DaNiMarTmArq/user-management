508a1a364ccc777d886f1179fd4a44728f77e3cd
Daniel Martinez <danimartinezmarquez@gmail.com>
2025-03-20 20:42:43 +0100

    Detail view

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, Input, signal } from '@angular/core';
+import { User } from '../../Interfaces/User';
+import { UserService } from '../../services/user.service';
+import { lastValueFrom } from 'rxjs';
+import { RouterModule } from '@angular/router';
+import { LoadComponent } from '../load/load.component';
 
 @Component({
   selector: 'app-detail',
-  imports: [],
+  imports: [RouterModule, LoadComponent],
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.css'
+  styleUrl: './detail.component.css',
 })
 export class DetailComponent {
+  userService = inject(UserService);
+  loading = signal(false);
+  user = signal<User | null>(null);
+  @Input() id: string = '';
 
+  async ngOnInit() {
+    this.loading.set(true);
+    const user = await lastValueFrom(this.userService.getUser(this.id));
+    this.user.set(user);
+    this.loading.set(false);
+  }
 }
