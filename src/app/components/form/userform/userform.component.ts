36acbdeb23aa75de3c6f1fbc333cddfd6a228119
Daniel <daniel.martinez@garajedeideas.com>
2025-03-21 11:27:32 +0100

    Logic for style the form differently depending of new / update logic

--- src/app/components/form/userform/userform.component.ts
+++ src/app/components/form/userform/userform.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+type RouteKey = 'newuser' | 'updateuser';
 
 @Component({
   selector: 'app-userform',
@@ -7,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrl: './userform.component.css'
 })
 export class UserformComponent {
+  route = inject(ActivatedRoute);
+  formMeta = {
+    title: 'Formulario',
+    submit: 'Enviar'
+  }
+  
 
+
+  readonly routeFormMeta: Record<RouteKey, { title: string; submit: string }> = {
+    newuser: { title: 'Nuevo usuario', submit: 'Guardar' },
+    updateuser: { title: 'Actualizar usuario', submit: 'Actualizar' }
+  };
+
+ ngOnInit() {
+    const route = this.route.snapshot.url[0]?.path;
+
+    if (route && (route in this.routeFormMeta)) {
+      this.formMeta = this.routeFormMeta[route as RouteKey];
+    }
+  }
 }
