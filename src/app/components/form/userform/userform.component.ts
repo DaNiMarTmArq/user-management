8b8a3ebd1e9e54c583364e12410deb2099d5bee1
Daniel <daniel.martinez@garajedeideas.com>
2025-03-21 13:55:25 +0100

    Form model added

--- src/app/components/form/userform/userform.component.ts
+++ src/app/components/form/userform/userform.component.ts
@@ -1,33 +1,44 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { last } from 'rxjs';
 
 type RouteKey = 'newuser' | 'updateuser';
 
 @Component({
   selector: 'app-userform',
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './userform.component.html',
   styleUrl: './userform.component.css'
 })
 export class UserformComponent {
   route = inject(ActivatedRoute);
-  formMeta = {
-    title: 'Formulario',
-    submit: 'Enviar'
-  }
-  
-
-
+ 
   readonly routeFormMeta: Record<RouteKey, { title: string; submit: string }> = {
     newuser: { title: 'Nuevo usuario', submit: 'Guardar' },
     updateuser: { title: 'Actualizar usuario', submit: 'Actualizar' }
   };
-
+  formMeta = {
+    title: 'Formulario',
+    submit: 'Enviar'
+  }
+  userForm = new FormGroup({
+    userName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    image: new FormControl(''),
+  })
+  
  ngOnInit() {
     const route = this.route.snapshot.url[0]?.path;
 
     if (route && (route in this.routeFormMeta)) {
       this.formMeta = this.routeFormMeta[route as RouteKey];
     }
+
+  }
+
+  handleSubmit() {
+    console.log(this.userForm.value);
   }
 }
