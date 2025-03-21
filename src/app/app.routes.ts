420826112112125f3c8e00c635fba6f8f97fb32a
Daniel <daniel.martinez@garajedeideas.com>
2025-03-21 11:05:03 +0100

    Form markup and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DetailComponent } from './components/detail/detail.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { UserformComponent } from './components/form/userform/userform.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: DashboardComponent, title: 'Dashboard' },
   { path: 'user/:id', component: DetailComponent, title: 'User detail' },
+  { path: 'newuser', component: UserformComponent, title: 'New user' },
   { path: '**', component: NotfoundComponent, title: 'Not found' },
 ];
